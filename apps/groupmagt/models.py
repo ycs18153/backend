9037af927cdc283822342deaf2621ddce2af366f
ycs18153 <twl223025@gmail.com>
2022-10-22 22:31:39 +0800

    add yaml file

---
+++ apps/groupmagt/models.py
@@ -0,0 +1,36 @@
+from multiprocessing import managers
+from typing import Optional
+import uuid
+from pydantic import BaseModel, Field
+
+
+class TaskModel(BaseModel):
+    groupId: str = Field(default_factory=uuid.uuid4, alias="_id")
+    group_name: str = Field(...)
+    group_managers: str = Field(...)
+    signup_date: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "groupId": "C8c55ed54b7107dd775c55b1483a7fef3",
+                "group_name": "R",
+                "group_managers": "TWC",
+                "signup_date": "2022-10-20-20:10:00",
+            }
+        }
+
+
+class UpdateTaskModel(BaseModel):
+    group_name: Optional[str]
+    managers: Optional[str]
+    transcation_date: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "My important task",
+                "completed": True,
+            }
+        }
---
+++ apps/groupmagt/routers.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, Body, Request, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+from .models import TaskModel, UpdateTaskModel
+
+router = APIRouter()
+
+
+@router.post("/", response_description="Add new group")
+async def create_group(request: Request, group: TaskModel = Body(...)):
+    group = jsonable_encoder(group)
+    new_group = await request.app.mongodb["group_id"].insert_one(group)
+    created_task = await request.app.mongodb["group_id"].find_one(
+        {"_id": new_group.inserted_id}
+    )
+
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_task)
+
+
+@router.get("/", response_description="List all groups")
+async def list_tasks(request: Request):
+    tasks = []
+    for doc in await request.app.mongodb["group_id"].find().to_list(length=100):
+        tasks.append(doc)
+    return tasks
+
+
+@router.get("/{id}", response_description="Get a specific group info")
+async def show_task(id: str, request: Request):
+    if (task := await request.app.mongodb["group_id"].find_one({"_id": id})) is not None:
+        return task
+
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+
+
+@router.put("/{id}", response_description="Update the specific group info")
+async def update_task(id: str, request: Request, group: UpdateTaskModel = Body(...)):
+    group = {k: v for k, v in group.dict().items() if v is not None}
+
+    if len(group) >= 1:
+        update_result = await request.app.mongodb["group_id"].update_one(
+            {"_id": id}, {"$set": group}
+        )
+
+        if update_result.modified_count == 1:
+            if (
+                updated_group := await request.app.mongodb["group_id"].find_one({"_id": id})
+            ) is not None:
+                return updated_group
+
+    if (
+        existing_group := await request.app.mongodb["group_id"].find_one({"_id": id})
+    ) is not None:
+        return existing_group
+
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+
+
+@router.delete("/{id}", response_description="Delete group")
+async def delete_task(id: str, request: Request):
+    delete_result = await request.app.mongodb["group_id"].delete_one({"_id": id})
+
+    if delete_result.deleted_count == 1:
+        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"Group {id} not found")
